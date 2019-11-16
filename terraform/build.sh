7bf7bcebc33daf97ba922b16d1e52410ab307598
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2019-11-16 11:17:20 +0100

    custom build for terratools with build_all.sh

--- terraform/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export TERRAFORM_VERSION=0.12.15
-export AWS_VAULT_VERSION=4.6.4
-export TERRAGRUNT_VERSION=0.19.14
-
-cd $(dirname $(basename $0))
-docker-compose  build terratools
---
+++ terraform/build_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# get latest tag from git-repositories
+export TERRAFORM_VERSION=$(git ls-remote --tags git://github.com/hashicorp/terraform.git | cut -d"/" -f3- | cut -b2-| grep -v "\^" |  sort -t. -k 1,1n -k 2,2n -k 3,3n | tail -1)
+export AWS_VAULT_VERSION=$(git ls-remote --tags git://github.com/99designs/aws-vault.git | cut -d"/" -f3- | cut -b2-| grep -v "-" | sort -t. -k 1,1n -k 2,2n -k 3,3n | tail -1)
+export TERRAGRUNT_VERSION=$(git ls-remote --tags git://github.com/gruntwork-io/terragrunt.git | cut -d"/" -f3- | cut -b2-| sort -t. -k 1,1n -k 2,2n -k 3,3n | tail -1)
+
+cd $(dirname $(basename $0))
+# Build latest version
+TERRAFORM_VERSION_latest=${TERRAFORM_VERSION}
+docker-compose build terratools
+
+for TERRAFORM_VERSION in $(cat terraform.version | grep -v ^# | grep -v ${TERRAFORM_VERSION_latest}) ; do
+   echo "Using Terraform: ${TERRAFORM_VERSION}"
+   export TERRAFORM_VERSION
+   docker-compose build terratools
+done
