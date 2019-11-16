b4d1c2f98358fe5e94710a2b9f762248a132877e
thorsten.bruhns@googlemail.com <thorsten.bruhns@googlemail.com>
2019-11-16 22:21:56 +0100

    support for docker pull

--- terraform/build_all.sh
+++ terraform/build_all.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 basename=$0
 
+docker_credentials=~/.docker/config.json
+docker_owner=rendanic
+
+work_docker() {
+
+   echo "#################################################"
+   echo "#################################################"
+   echo "Using Terraform: ${TERRAFORM_VERSION}"
+   docker-compose build terratools
+
+   dockerimage="terratools:${TERRAFORM_VERSION}"
+   docker tag ${dockerimage} rendanic/${dockerimage}
+   test -f "$docker_credentials" && docker push ${docker_owner}/${dockerimage}
+}
+
+if [ "$dockerpass" -a "$dockeruser" ] ; then
+   echo $dockerpass | docker login --password-stdin -u $dockeruser || rm -f "$docker_credentials"
+fi
+
 # get latest tag from git-repositories
 export TERRAFORM_VERSION=$(git ls-remote --tags git://github.com/hashicorp/terraform.git | cut -d"/" -f3- | cut -b2-| grep -v "\^" |  sort -t. -k 1,1n -k 2,2n -k 3,3n | tail -1)
 export AWS_VAULT_VERSION=$(git ls-remote --tags git://github.com/99designs/aws-vault.git | cut -d"/" -f3- | cut -b2-| grep -v "-" | sort -t. -k 1,1n -k 2,2n -k 3,3n | tail -1)
@@ -9,16 +28,13 @@ export TERRAGRUNT_VERSION=$(git ls-remote --tags git://github.com/gruntwork-io/t
 cd $(dirname $basename)
 # Build latest version
 TERRAFORM_VERSION_latest=${TERRAFORM_VERSION}
-
-echo "#################################################"
-echo "#################################################"
-echo "Using Terraform: ${TERRAFORM_VERSION}"
-docker-compose build terratools || exit 10
+work_docker
 
 for TERRAFORM_VERSION in $(cat terraform.version | grep -v ^# | grep -v ${TERRAFORM_VERSION_latest}) ; do
-   echo "#################################################"
-   echo "#################################################"
    echo "Using Terraform: ${TERRAFORM_VERSION}"
    export TERRAFORM_VERSION
-   docker-compose build terratools || exit 10
+   work_docker
 done
+
+echo "Remove Docker credentials"
+test -f "$docker_credentials" && rm -f "$docker_credentials"
