478c8d713e0d27732e98600d0221462953f1639f
Thorsten Bruhns <Thorsten.Bruhns@googlemail.com>
2020-05-21 04:51:59 +0000

    ansibleaws: New Container for Ansible + AWS-Tools

---
+++ ansibleaws/build_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+dir=$(dirname $(basename $0))
+
+for stable in $(git ls-remote --heads git://github.com/ansible/ansible.git | cut -d"/" -f3- | grep "^stable-2.[2-9]") ; do
+   "$dir/build.sh" "$stable"
+done
