469fdd32aa3635f19428ea03dbb78d0ce8796e69
Thorsten Bruhns <thorsten.bruhns@googlemail.com>
2020-05-25 22:43:51 +0200

    github actions

--- ansibleaws/build_all.sh
+++ ansibleaws/build_all.sh
@@ -5,5 +5,6 @@
 dir=$(dirname $(basename $0))
 
 for stable in $(git ls-remote --heads git://github.com/ansible/ansible.git | cut -d"/" -f3- | grep "^stable-2.[2-9]") ; do
-   "$dir/build.sh" "$stable"
+#   "$dir/build.sh" "$stable"
+   "ansibleaws/build.sh" "$stable"
 done
