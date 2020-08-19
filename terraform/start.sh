47bcd88ef65a8bc649f91aa8645e0bc1f5b2f50f
Thorsten.Bruhns@googlemail.com <Thorsten.Bruhns@googlemail.com>
2020-08-19 08:22:03 +0000

    more flexibility in start.sh

--- terraform/start.sh
+++ terraform/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 export TERRAFORM_VERSION=$1
-
+export containeruser="${containeruser:-"$USER"}"
+export userhome=$(getent passwd | grep "^${containeruser}:" | cut -d":" -f6 )
+export TERRAGRUNT_DOWNLOAD="${userhome}/.terragrunt_cache_docker"
 wdir=$(dirname $0)
 echo ${wdir}
 
 cd $wdir
-docker-compose run --rm -u ${containeruser:-"$USER"} -e HOME=/home/terraform -w ${2:-"/home/terraform/"} terratools bash
+docker-compose run --rm -u ${containeruser} -e HOME="${userhome}" -e TERRAGRUNT_DOWNLOAD="$TERRAGRUNT_DOWNLOAD" -w ${2:-"/home/terraform/"} terratools bash
